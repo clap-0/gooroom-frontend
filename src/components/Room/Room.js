f27d504d00483343f63c30d97c7049ab52744bc9
clap_0 <clap-0@naver.com>
2023-06-06 00:16:50 +0900

    fix: address가 undefined일 때 geocoder를 사용하지 않도록 수정

--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -12,20 +12,22 @@ const Room = ({address}) => {
     };
 
     const map = new kakao.maps.Map(container, options);
-    //위도, 경도로 변환 및 마커표시
-    var geocoder = new kakao.maps.services.Geocoder();
-    geocoder.addressSearch(address, (result, status) => {
-      if (status === kakao.maps.services.Status.OK) {
-        var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+    if (address) {
+      //위도, 경도로 변환 및 마커표시
+      var geocoder = new kakao.maps.services.Geocoder();
+      geocoder.addressSearch(address, (result, status) => {
+        if (status === kakao.maps.services.Status.OK) {
+          var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-        var markerPosition = new kakao.maps.LatLng(result[0].y, result[0].x);
-        var marker = new kakao.maps.Marker({
-          position: markerPosition,
-        });
-        marker.setMap(map);
-        map.setCenter(coords);
-      }
-    });
+          var markerPosition = new kakao.maps.LatLng(result[0].y, result[0].x);
+          var marker = new kakao.maps.Marker({
+            position: markerPosition,
+          });
+          marker.setMap(map);
+          map.setCenter(coords);
+        }
+      });
+    }
   }, [address]);
 
   return (
