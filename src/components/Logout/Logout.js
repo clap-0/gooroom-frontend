7a70381e2d3ba0cd097dc19b169fb1058abbfdf7
clap_0 <clap-0@naver.com>
2023-06-06 23:52:10 +0900

    fix: 로그아웃 중 로딩 화면을 출력하도록 수정

--- src/components/Logout/Logout.js
+++ src/components/Logout/Logout.js
@@ -1,8 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import Loading from 'components/common/Loading/Loading';
 import {API_LOGOUT} from 'constants/apiUrls';
 import useAlert from 'hooks/useAlert';
 import useInterceptedAxios from 'hooks/useInterceptedAxios';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import {useResetRecoilState} from 'recoil';
 import {AuthState} from 'stores/AuthState';
@@ -28,9 +29,17 @@ const Logout = () => {
     }
   };
 
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     logout();
+    setLoading(false);
   }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
     <>
